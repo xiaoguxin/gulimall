c214a5d6d350f4e17f1ffa9b1c7d1832d7261a5b
sunny <675326787@qq.com>
2022-01-15 20:27:32 +0800

    update:product模块获取商品分类树

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Service("categoryService")
@@ -30,7 +31,31 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
     @Override
     public List<CategoryEntity> findListTree() {
         //1.查询出所有分类
-        List<CategoryEntity> categoryEntityList=baseMapper.selectList(null);
-        return categoryEntityList;
+        List<CategoryEntity> categoryList = baseMapper.selectList(null);
+
+        //1.1查询出所有的一级分类
+        List<CategoryEntity> categoryParentList = categoryList.stream()
+                .filter(c -> c.getParentCid().equals(0L))
+                .map((c) -> {
+                    c.setChildren(getChildrenList(c, categoryList));
+                    return c;
+                }).sorted((c1, c2) -> {
+                    return (c1.getSort() == null ? 0 : c1.getSort()) - (c2.getSort() == null ? 0 : c2.getSort());
+                }).collect(Collectors.toList());
+
+
+        return categoryParentList;
+    }
+
+    private List<CategoryEntity> getChildrenList(CategoryEntity root, List<CategoryEntity> all) {
+        List<CategoryEntity> children = all.stream().
+                filter(c -> root.getCatId().equals(c.getParentCid()))
+                .map((c) -> {
+                    c.setChildren(getChildrenList(c, all));
+                    return c;
+                }).sorted((c1, c2) -> {
+                    return (c1.getSort() == null ? 0 : c1.getSort()) - (c2.getSort() == null ? 0 : c2.getSort());
+                }).collect(Collectors.toList());
+        return children;
     }
 }
