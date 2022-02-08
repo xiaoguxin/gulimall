00e9472712e04c6427f7ef7c1d0cf8b8fd28e209
sunny <2377455485@qq.com>
2022-02-08 12:08:36 +0800

    update:获取分类关联品牌列表

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryBrandRelationController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryBrandRelationController.java
@@ -2,8 +2,10 @@ package com.atguigu.gulimall.product.controller;
 
 import com.atguigu.common.utils.PageUtils;
 import com.atguigu.common.utils.R;
+import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.entity.CategoryBrandRelationEntity;
 import com.atguigu.gulimall.product.service.CategoryBrandRelationService;
+import com.atguigu.gulimall.product.vo.BrandVo;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-
+import java.util.stream.Collectors;
 
 
 /**
@@ -39,6 +41,29 @@ public class CategoryBrandRelationController {
         );
 
         return R.ok().put("data", data);
+    }
+
+    /**
+     *  /product/categorybrandrelation/brands/list
+     *
+     *  1、Controller：处理请求，接受和校验数据
+     *  2、Service接受controller传来的数据，进行业务处理
+     *  3、Controller接受Service处理完的数据，封装页面指定的vo
+     */
+    @GetMapping("/brands/list")
+    public R relationBrandsList(@RequestParam(value = "catId",required = true)Long catId){
+        List<BrandEntity> vos = categoryBrandRelationService.getBrandsByCatId(catId);
+
+        List<BrandVo> collect = vos.stream().map(item -> {
+            BrandVo brandVo = new BrandVo();
+            brandVo.setBrandId(item.getBrandId());
+            brandVo.setBrandName(item.getName());
+
+            return brandVo;
+        }).collect(Collectors.toList());
+
+        return R.ok().put("data",collect);
+
     }
 
     /**
