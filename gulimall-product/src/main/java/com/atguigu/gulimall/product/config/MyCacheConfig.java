880f1345aa939f4792ad98957238536f1914e4cd
sunny <2377455485@qq.com>
2022-03-09 22:25:07 +0800

    add:将cache配置文件中的所有配置都生效

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/config/MyCacheConfig.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/config/MyCacheConfig.java
@@ -1,5 +1,8 @@
 package com.atguigu.gulimall.product.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,18 +11,48 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@EnableConfigurationProperties(CacheProperties.class)
 @Configuration
 @EnableCaching
 public class MyCacheConfig {
 
+    /*@Autowired
+    CacheProperties cacheProperties;*/
+
+    /**
+     * 配置文件中的东西没有用上：
+     * 1、原来和配置文件绑定的配置类是这样子的
+     *      @ConfigurationProperties(prefix = "spring.cache")
+     *      public class CacheProperties
+     * 2、要让他生效
+     * @return
+     */
     @Bean
-    RedisCacheConfiguration redisCacheConfiguration(){
+    RedisCacheConfiguration redisCacheConfiguration(CacheProperties cacheProperties){
 
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
         //config = config.entryTtl();
         config = config.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()));
 
         config = config.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        //将配置文件中的所有配置都生效
+        CacheProperties.Redis redisProperties = cacheProperties.getRedis();
+        if (redisProperties.getTimeToLive() != null) {
+            config = config.entryTtl(redisProperties.getTimeToLive());
+        }
+
+        if (redisProperties.getKeyPrefix() != null) {
+            config = config.prefixCacheNameWith(redisProperties.getKeyPrefix());
+        }
+
+        if (!redisProperties.isCacheNullValues()) {
+            config = config.disableCachingNullValues();
+        }
+
+        if (!redisProperties.isUseKeyPrefix()) {
+            config = config.disableKeyPrefix();
+        }
         return config;
 
     }
