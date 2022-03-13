2f597eafbe5ce1f80d6e6dbf853b1a1490e3685d
sunny <2377455485@qq.com>
2022-03-13 22:20:00 +0800

    update：检索添加页面查询接收对象和返回参数对象

---
+++ gulimall-search/src/main/java/com/atguigu/gulimall/search/service/impl/MallSearchServiceImpl.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.search.service.impl;
+
+import com.atguigu.gulimall.search.service.MallSearchService;
+import com.atguigu.gulimall.search.vo.SearchParam;
+import com.atguigu.gulimall.search.vo.SearchResult;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MallSearchServiceImpl implements MallSearchService {
+
+    @Override
+    public SearchResult search(SearchParam param) {
+        return null;
+    }
+
+
+}
---
+++ gulimall-search/src/main/java/com/atguigu/gulimall/search/vo/SearchParam.java
@@ -0,0 +1,44 @@
+package com.atguigu.gulimall.search.vo;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * 封装页面所有可能传递过来的查询条件
+ *
+ * catalog3Id=225&keyword=小米&sort=saleCount_asc&hasStock=0/1&brandId=1&brandId=2
+ */
+@Data
+public class SearchParam {
+
+    private String keyword;//页面传递过来的全文匹配关键字
+
+    private Long catalog3Id;//三级分类id
+
+    /**
+     *  sort=saleCount_asc/desc
+     *  sort=skuPrice_asc/desc
+     *  sort=hotScore_asc/desc
+     */
+    private String sort;//排序条件
+
+    /**
+     * 好多的过滤条件
+     * hasStock(是否有货)、skuPrice区间、brandId、catalog3Id、attrs
+     * hasStock=0/1
+     * skuPrice=1_500/_500/500_
+     * brandId=1
+     * attrs=2_5寸：6寸
+     */
+    private Integer hasStock;//是否只显示有货
+    private String skuPrice;//价格区间查询
+    private List<Long> brandId;//按照品牌进行查询，可以多选
+    private List<String> attrs;//按照属性进行筛选
+    private Integer pageNum;//页码
+
+
+
+
+
+}
