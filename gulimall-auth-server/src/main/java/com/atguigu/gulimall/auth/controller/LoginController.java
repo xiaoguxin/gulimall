30440bd9c7d0257f6877c7fd34ae9c3c565ac568
sunny <2377455485@qq.com>
2022-03-27 18:40:51 +0800

    add:页面跳转配置

--- gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/controller/LoginController.java
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/controller/LoginController.java
@@ -7,7 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-    @GetMapping("/login.html")
+    /**
+     * 发送一个请求跳转到一个页面
+     * SpringMVC viewcontroller 将请求和页面映射过来
+     */
+
+    /*@GetMapping("/login.html")
     public String loginPage(){
         return "login";
     }
@@ -15,5 +20,5 @@ public class LoginController {
     @GetMapping("/reg.html")
     public String regPage(){
         return "reg";
-    }
+    }*/
 }
