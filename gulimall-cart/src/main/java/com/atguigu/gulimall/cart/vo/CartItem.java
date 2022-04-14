51e6dc15529d83e726dbabe985c1a25778e4bfaa
sunny <2377455485@qq.com>
2022-04-14 21:02:18 +0800

    update:购物车VO编写

---
+++ gulimall-cart/src/main/java/com/atguigu/gulimall/cart/vo/CartItem.java
@@ -0,0 +1,77 @@
+package com.atguigu.gulimall.cart.vo;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * 购物项内容
+ */
+public class CartItem {
+    private Long skuId;
+    private Boolean check = true;
+    private String title;
+    private List<String> skuAttr;
+    private BigDecimal price;
+    private Integer count;
+    private BigDecimal totalPrice;
+
+    public Long getSkuId() {
+        return skuId;
+    }
+
+    public void setSkuId(Long skuId) {
+        this.skuId = skuId;
+    }
+
+    public Boolean getCheck() {
+        return check;
+    }
+
+    public void setCheck(Boolean check) {
+        this.check = check;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<String> getSkuAttr() {
+        return skuAttr;
+    }
+
+    public void setSkuAttr(List<String> skuAttr) {
+        this.skuAttr = skuAttr;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    /**
+     * 计算当前项的总价
+     * @return
+     */
+    public BigDecimal getTotalPrice() {
+        return this.price.multiply(new BigDecimal(""+this.count));
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
