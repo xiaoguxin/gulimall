90fa1bdb37b3a1792c99957227626878aa49e932
sunny <2377455485@qq.com>
2022-04-14 22:39:07 +0800

    add:cart服务购物车user-key封装，使用拦截器和ThreadLocal

---
+++ gulimall-cart/src/main/java/com/atguigu/gulimall/cart/controller/CartController.java
@@ -0,0 +1,33 @@
+package com.atguigu.gulimall.cart.controller;
+
+import com.atguigu.common.constant.AuthServerConstant;
+import com.atguigu.gulimall.cart.interceptor.CartInterceptor;
+import com.atguigu.gulimall.cart.vo.UserInfoTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class CartController {
+
+    /**
+     * 浏览器有一个cookie;user-key;标识用户身份，一个月后过期；
+     * 如果第一次使用jd的购物车功能，都会给一个临时的用户身份；
+     * 浏览器以后保存，每次访问都会带上这个cookie;
+     *
+     * 登录：session有
+     * 没登录：按照cookie里面带来user-key来做。
+     * 第一次：如果没有临时用户，帮忙创建一个临时用户。
+     * @return
+     */
+    @GetMapping("/cart.html")
+    public String cartListPage(){
+
+        //1、快速得到用户信息，id,user-key
+        UserInfoTo userInfoTo = CartInterceptor.threadLocal.get();
+        System.out.println(userInfoTo);
+
+        return "cartList";
+    }
+}
---
+++ gulimall-cart/src/main/java/com/atguigu/gulimall/cart/vo/UserInfoTo.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.cart.vo;
+
+import lombok.Data;
+import lombok.ToString;
+
+@ToString
+@Data
+public class UserInfoTo {
+
+    private Long userId;
+    private String userKey; //一定封装
+
+    private boolean tempUser = false;
+
+
+
+}
