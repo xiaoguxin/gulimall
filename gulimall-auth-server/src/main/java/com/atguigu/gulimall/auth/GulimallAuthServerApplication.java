d52e1edd5b99e12021277a33666fec4060f685af
sunny <2377455485@qq.com>
2022-04-20 21:19:03 +0800

    add:spring session原理

--- gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/GulimallAuthServerApplication.java
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/GulimallAuthServerApplication.java
@@ -6,6 +6,21 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+/**
+ * 核心原理
+ * 1）、@EnableRedisHttpSession导入RedisHttpSessionConfiguration配置
+ *      1、给容器中添加一个组件
+ *          SessionRepository=》》》【RedisOperationSessionRepository】=》》redis操作session.session的增删改查封装类
+ *      2、SessionRepositoryFilter==》Filter:session存储过滤器;每个请求过来都必须经过filler
+ *          1、创建的时候，就自动从容器中获取到了SessionRepository;
+ *          2、原始的request,response都被包装。SessionRepositoryRequestWrapper;SessionRepositoryResponseWrapper;
+ *          3、以后获取session。request.getSession();
+ *          4、WrapperRequest.getSession();===> SessionRepository中获取到的。
+ *
+ *     装饰者模式
+ *
+ *     自动延期；redis中的数据也是有过期时间的
+ */
 @EnableRedisHttpSession //整合redis作为session存储
 @EnableFeignClients
 @EnableDiscoveryClient
