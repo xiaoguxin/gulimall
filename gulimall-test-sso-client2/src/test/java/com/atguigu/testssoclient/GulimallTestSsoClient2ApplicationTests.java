edaaa7f9e3ef7bab030a30304c48a04880d1e702
sunny <2377455485@qq.com>
2022-04-23 13:48:59 +0800

    add:单点登录测试逻辑

---
+++ gulimall-test-sso-client2/src/test/java/com/atguigu/testssoclient/GulimallTestSsoClient2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.testssoclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallTestSsoClient2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
