ea77955833dc2ac021ebc855ff2573192146b70d
sunny <2377455485@qq.com>
2022-05-09 21:31:19 +0800

    add:直接接收消息对象

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/service/impl/OrderItemServiceImpl.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/service/impl/OrderItemServiceImpl.java
@@ -1,8 +1,12 @@
 package com.atguigu.gulimall.order.service.impl;
 
+import com.atguigu.gulimall.order.entity.OrderReturnReasonEntity;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+
+import java.nio.channels.Channel;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -34,12 +38,25 @@ public class OrderItemServiceImpl extends ServiceImpl<OrderItemDao, OrderItemEnt
      * org.springframework.amqp.core.Message
      *
      * 参考可以写一下类型
+     * 1、Message message:原生消息详细信息。头+体
+     * 2、T<发送的消息的类型> OrderReturnReasonEntity content
+     * 3、Channel channel: 当前传输数据的通道
+     *
+     * Queue: 可以很多人都来监听。只要收到消息，队列删除消息，而且只能有一个收到此消息
+     * 场景：
+     *      1）、订单服务启动多个;同一个消息，只能有一个客户端收到
+     *      2）、
      * @param message
      */
     @RabbitListener(queues = {"hello-java-queue"})
-    public void recieveMessage(Message message){
+    public void recieveMessage(Message message,
+                               OrderReturnReasonEntity content,
+                               Channel channel){
+        //{"id":1,"name":"哈哈","sort":null,"status":null,"createTime":1652100907404}
         byte[] body = message.getBody();
-        System.out.println("接收到消息...内容："+message+"==>类型："+message.getClass());
+        //消息头属性信息
+        MessageProperties properties = message.getMessageProperties();
+        System.out.println("接收到消息..."+message+"==>内容："+content);
     }
 
 }
