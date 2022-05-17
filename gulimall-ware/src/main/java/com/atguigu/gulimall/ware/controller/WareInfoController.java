a2184f97a8be55db6805eac563ec9ed7641b3a4d
sunny <2377455485@qq.com>
2022-05-17 22:55:10 +0800

    add:模拟运费计算（暂时使用手机尾号）

--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/controller/WareInfoController.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/controller/WareInfoController.java
@@ -1,14 +1,11 @@
 package com.atguigu.gulimall.ware.controller;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.atguigu.gulimall.ware.entity.WareInfoEntity;
 import com.atguigu.gulimall.ware.service.WareInfoService;
@@ -27,63 +24,69 @@ import com.atguigu.common.utils.R;
 @RestController
 @RequestMapping("ware/wareinfo")
 public class WareInfoController {
-    @Autowired
-    private WareInfoService wareInfoService;
+        @Autowired
+        private WareInfoService wareInfoService;
 
-    /**
-     * 列表
-     */
-    @RequestMapping("/list")
-    //@RequiresPermissions("ware:wareinfo:list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = wareInfoService.queryPage(params);
+        @GetMapping("/fare")
+        public R getFare(@RequestParam("addrId") Long addrId){
+            BigDecimal fare = wareInfoService.getFare(addrId);
+            return R.ok().setData(fare);
+        }
 
-        return R.ok().put("page", page);
-    }
+        /**
+         * 列表
+         */
+        @RequestMapping("/list")
+        //@RequiresPermissions("ware:wareinfo:list")
+        public R list(@RequestParam Map<String, Object> params){
+            PageUtils page = wareInfoService.queryPage(params);
+
+            return R.ok().put("page", page);
+        }
 
 
-    /**
-     * 信息
-     */
-    @RequestMapping("/info/{id}")
-    //@RequiresPermissions("ware:wareinfo:info")
-    public R info(@PathVariable("id") Long id){
-		WareInfoEntity wareInfo = wareInfoService.getById(id);
+        /**
+         * 信息
+         */
+        @RequestMapping("/info/{id}")
+        //@RequiresPermissions("ware:wareinfo:info")
+        public R info(@PathVariable("id") Long id){
+            WareInfoEntity wareInfo = wareInfoService.getById(id);
 
-        return R.ok().put("wareInfo", wareInfo);
-    }
+            return R.ok().put("wareInfo", wareInfo);
+        }
 
-    /**
-     * 保存
-     */
-    @RequestMapping("/save")
-    //@RequiresPermissions("ware:wareinfo:save")
-    public R save(@RequestBody WareInfoEntity wareInfo){
-		wareInfoService.save(wareInfo);
+        /**
+         * 保存
+         */
+        @RequestMapping("/save")
+        //@RequiresPermissions("ware:wareinfo:save")
+        public R save(@RequestBody WareInfoEntity wareInfo){
+            wareInfoService.save(wareInfo);
 
-        return R.ok();
-    }
+            return R.ok();
+        }
 
-    /**
-     * 修改
-     */
-    @RequestMapping("/update")
-    //@RequiresPermissions("ware:wareinfo:update")
-    public R update(@RequestBody WareInfoEntity wareInfo){
-		wareInfoService.updateById(wareInfo);
+        /**
+         * 修改
+         */
+        @RequestMapping("/update")
+        //@RequiresPermissions("ware:wareinfo:update")
+        public R update(@RequestBody WareInfoEntity wareInfo){
+            wareInfoService.updateById(wareInfo);
 
-        return R.ok();
-    }
+            return R.ok();
+        }
 
-    /**
-     * 删除
-     */
-    @RequestMapping("/delete")
-    //@RequiresPermissions("ware:wareinfo:delete")
-    public R delete(@RequestBody Long[] ids){
-		wareInfoService.removeByIds(Arrays.asList(ids));
+        /**
+         * 删除
+         */
+        @RequestMapping("/delete")
+        //@RequiresPermissions("ware:wareinfo:delete")
+        public R delete(@RequestBody Long[] ids){
+            wareInfoService.removeByIds(Arrays.asList(ids));
 
-        return R.ok();
-    }
+            return R.ok();
+        }
 
 }
