6ca749ee268977c1578e71172fbbad05bd147555
sunny <2377455485@qq.com>
2022-05-19 22:49:56 +0800

    add:订单token防重

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/web/OrderWebController.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/web/OrderWebController.java
@@ -2,10 +2,13 @@ package com.atguigu.gulimall.order.web;
 
 import com.atguigu.gulimall.order.service.OrderService;
 import com.atguigu.gulimall.order.vo.OrderConfirmVo;
+import com.atguigu.gulimall.order.vo.OrderSubmitVo;
+import com.atguigu.gulimall.order.vo.SubmitOrderResponseVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.concurrent.ExecutionException;
@@ -25,6 +28,27 @@ public class OrderWebController {
 
         //展示订单确认的数据
         return "confirm";
+    }
+
+    /**
+     * 下单功能
+     * @param vo
+     * @return
+     */
+    @PostMapping("/submitOrder")
+    public String submitOrder(OrderSubmitVo vo){
+
+        SubmitOrderResponseVo responseVo = orderService.submitOrder(vo);
+
+        System.out.println("订单提交的数据..."+vo);
+        if(responseVo.getCode() == 0){
+            //下单成功来到支付选择页
+            return
+        }else{
+            //下单失败回到订单确认页重新确认订单信息
+            return "redirect:http://order.mall.com/toTrade";
+        }
+        return null;
     }
 
 }
