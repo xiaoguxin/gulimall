8e71194c0c5980ef6608a5f5146bde8d52ac6ce2
sunny <2377455485@qq.com>
2022-05-28 21:28:57 +0800

    add:创建业务交换机&队列及绑定关系，注意需要有消费者才会自动创建

---
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/config/MyRabbitConfig.java
@@ -0,0 +1,80 @@
+package com.atguigu.gulimall.ware.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class MyRabbitConfig {
+
+    /**
+     * 使用JSON序列化机制，进行消息转换
+     *
+     * @return
+     */
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @RabbitListener(queues = "stock.release.stock.queue")
+    public void handle(Message message) {
+
+    }
+
+    @Bean
+    public Exchange stockEventExchange() {
+        //String name, boolean durable, boolean autoDelete, Map<String, Object> arguments
+        return new TopicExchange("stock-event-exchange", true, false);
+    }
+
+    @Bean
+    public Queue stockReleaseStockQueue() {
+        //String name, boolean durable, boolean exclusive, boolean autoDelete, Map<String, Object> arguments
+        return new Queue("stock.release.stock.queue", true, false, false);
+    }
+
+    @Bean
+    public Queue stockDelayQueue() {
+        Map<String, Object> args = new HashMap<>();
+        /**
+         * x-dead-letter-exchange:stock-event-exchange
+         * x-dead-letter-routing-key:stock.release
+         * x-message-ttl:120000
+         */
+        args.put("x-dead-letter-exchange", "stock-event-exchange");
+        args.put("x-dead-letter-routing-key", "stock.release");
+        args.put("x-message-ttl", 120000);
+        //String name, boolean durable, boolean exclusive, boolean autoDelete, Map<String, Object> arguments
+        return new Queue("stock.delay.queue", true, false, false, args);
+    }
+
+    @Bean
+    public Binding stockReleaseBinding() {
+        //String destination, Binding.DestinationType destinationType, String exchange, String routingKey, Map<String, Object> arguments
+        return new Binding(
+                "stock.release.stock.queue",
+                Binding.DestinationType.QUEUE,
+                "stock-event-exchange",
+                "stock.release.#",
+                null);
+    }
+
+    @Bean
+    public Binding stockLockedBinding() {
+        //String destination, Binding.DestinationType destinationType, String exchange, String routingKey, Map<String, Object> arguments
+        return new Binding(
+                "stock.delay.queue",
+                Binding.DestinationType.QUEUE,
+                "stock-event-exchange",
+                "stock.locked",
+                null);
+    }
+
+}
